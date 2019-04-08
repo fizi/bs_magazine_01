6276abc09057101b30781562981f62e5fa639740
fizi <fizi@wsb.hu>
2019-04-08 21:24:12 +0200

    initial file

---
+++ languages/English.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2012 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Bootstrap Theme Language file for e107 v2+
+ *
+*/
+
+
+define("LAN_THEME_1", "Comments:");
+define("LAN_THEME_2", "Comments are turned off for this item");
+define("LAN_THEME_3", "read more");
+define("LAN_THEME_4", "Posted by");
+define("LAN_THEME_5", " on ");
+define("LAN_THEME_6", "Theme by: <a href='http://fizithemes.hu'><b>FIZI THEMES</b></a>");
+define("LAN_THEME_7", "Trackback: ");
+define("LAN_THEME_8", "Posted by ");
+define("LAN_THEME_9", "Category:");
+
+/* HITS COUNTER */
+define("LAN_THEME_50", "Views:");
+
+/* TAGS LAN */
+define("LAN_THEME_60", "Tags:");
+
+/* ABOUT LAN */
+define("LAN_THEME_70", "About the Author");
+define("LAN_THEME_71", "My Articles");
+
+/* newsticker */
+define("LAN_THEME_100", "Quick News:");
+
+/* LAN SOCIAL MEDIA BUTTONS ON RIGHT SIDE  */
+define("LAN_THEME_200", "Follow on Facebook");
+define("LAN_THEME_201", "Follow on Twitter");
+define("LAN_THEME_202", "+1 on Google");
+
+/* LAN TABBED MENUS  */
+define("LAN_THEME_300", "Latest");
+define("LAN_THEME_301", "Popular");
+define("LAN_THEME_302", "Comments");
+
+?>
---
+++ languages/Hungarian.php
@@ -0,0 +1,45 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2015/09/04 20:11:24
+|
+|        $Author: fizi $
++---------------------------------------------------------------+
+*/
+define("LAN_THEME_1", "Hozzászólás:");
+define("LAN_THEME_2", "Hozzászólás kikapcsolva!");
+define("LAN_THEME_3", "tovább");
+define("LAN_THEME_4", "Írta:");
+define("LAN_THEME_5", " - ");
+define("LAN_THEME_6", "Theme: <a href='http://fizithemes.hu'><b>FIZI THEMES</b></a>");
+define("LAN_THEME_7", "Visszamutatás (Trackback):");
+define("LAN_THEME_8", "Beküldte:");
+define("LAN_THEME_9", "Kategória:");
+
+/* HITS COUNTER */
+define("LAN_THEME_50", "Megtekintés:");
+
+/* TAGS LAN */
+define("LAN_THEME_60", "Cimkék:");
+
+/* ABOUT LAN */
+define("LAN_THEME_70", "A Szerzőről");
+define("LAN_THEME_71", "A Szerző cikkei");
+
+/* newsticker */
+define("LAN_THEME_100", "Gyorshírek:");
+
+/* LAN SOCIAL MEDIA BUTTONS ON RIGHT SIDE  */
+define("LAN_THEME_200", "Facebook megosztás");
+define("LAN_THEME_201", "Twitter követés");
+define("LAN_THEME_202", "+1 a Google-n");
+
+/* LAN TABBED MENUS  */
+define("LAN_THEME_300", "Legújabb");
+define("LAN_THEME_301", "Népszerű");
+define("LAN_THEME_302", "Hozzászólás");
+
+?>
